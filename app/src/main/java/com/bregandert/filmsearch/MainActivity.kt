003a158675eaa1e56e760e60a7446d727df007b6
Bregandert <bregandert84@gmail.com>
2023-08-01 17:50:13 +0300

    Module 20

--- app/src/main/java/com/bregandert/filmsearch/MainActivity.kt
+++ app/src/main/java/com/bregandert/filmsearch/MainActivity.kt
@@ -2,10 +2,40 @@ package com.bregandert.filmsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bregandert.filmsearch.databinding.ActivityMainBinding
+
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        setContentView(R.layout.activity_main)
+
+        initButtons()
+
+    }
+
+    private fun initButtons() {
+        binding.btnMenu.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnFavorites.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnWatchLater.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnSelections.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnSettings.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
